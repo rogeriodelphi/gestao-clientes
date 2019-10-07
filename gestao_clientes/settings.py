40e9c7d87baad77a67f604ad4860ccf8e7770ab0
rogeriodelphi1 <rbm5@ufpe.cin.br>
2019-10-07 16:48:28 -0300

    Corrigindo Erro com o footer da pagina de Login e Person

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
+STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'
@@ -132,5 +134,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     'statics',
 ]
-
-STATIC_URL = '/static/'
