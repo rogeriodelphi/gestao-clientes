6264f08a06438c7145d0370c07e1516e9062dfc1
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-10-08 11:40:59 -0300

    Adicionando o models Venda

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Documento, Venda
 
 admin.site.register(Person)
-
+admin.site.register(Documento)
+admin.site.register(Venda)
 # Register your models here.
